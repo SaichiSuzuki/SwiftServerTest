43f65ca7fa3957ee507716cc97cf3ea7e4fbb8b1
zebra <cisa12076@g.nihon-u.ac.jp>
2016-03-28 17:20:09 +0900

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Kituraを使ったswiftによるサーバサイド
+
+1. `$ make`
+
+2. `$ .build/debug/KituraThird`
--- Sources/main.swift
+++ Sources/main.swift
@@ -9,7 +9,7 @@ let router = Router()
             response.send("Hello, World!")
             /* response.send("Hello, World!") */
             response.status(HttpStatusCode.OK).send("ouch")
-            response.status(HttpStatusCode.OK).send("<p>ouch</p>")
+            /* response.status(HttpStatusCode.OK).send("<p>ouch</p>") */
             /* response.status(HttpStatusCode.OK).send("<html><body bgcolor='skyblue'><body></html>") */
             /* response.appendBodyString("<html><body style=\"background: rgb(\(request.urlVariables["red"] ?? "0"), \(request.urlVariables["blue"] ?? "0"), \(request.urlVariables["green"] ?? "0"))\">\n */
             next()
